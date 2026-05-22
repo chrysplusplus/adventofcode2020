45752d552262b4e68bd00e242a61deabb5d7b175
Chrys <bagwellchristopher@hotmail.co.uk>
2026-05-22 09:01:09 +0100

    Incomplete day 14

--- 14.hs
+++ 14.hs
@@ -9,6 +9,8 @@ type UInt64 = Word64
 data Instruction = SetMask String
                  | SetMem Word64 Word64
 
+rightarr = right . arr
+
 tokens :: String -> [String]
 tokens = splitTokens [] ""
   where
@@ -16,7 +18,6 @@ tokens = splitTokens [] ""
     appendToken tkns tkn = tkns ++ [tkn]
     appendToken2 tkns "" tkn2   = tkns ++ [tkn2]
     appendToken2 tkns tkn1 tkn2 = tkns ++ [tkn1,tkn2]
-
     splitTokens tkns cur_tkn "" = appendToken tkns cur_tkn
     splitTokens tkns cur_tkn (x:xs) = case x of
       ' ' -> splitTokens (appendToken tkns cur_tkn) "" xs
@@ -25,14 +26,35 @@ tokens = splitTokens [] ""
       _   -> splitTokens tkns (cur_tkn ++ [x]) xs
 
 parseInstruction :: String -> Maybe Instruction
-parseInstruction = arr tokens >>>
-  isSetMask >>> (isSetMem >>> noInstruction ||| mkSetMem) ||| mkSetMask
-    where
-      isSetMask = arr head &&& arr id >>>
-        arr (\(instruction,s) -> if instruction == "mask" then Left s else Right s)
-      mkSetMask =
-      isSetMem = arr head &&& arr id >>>
-        arr (\(instruction,s) -> if instruction == "mem" then Left () else Right s)
+parseInstruction = arr tokens >>> arr matchInstruction
+  where
+    mEq tkns = case tkns of
+      []     -> Left ()
+      (x:xs) -> if (x == "=") then Right xs else Left ()
+    mSbl tkns = case tkns of
+      []     -> Left ()
+      (x:xs) -> if (x == "[") then Right xs else Left ()
+    mSbr tkns = case tkns of
+      []     -> Left ()
+      (x:xs) -> if (x == "]") then Right xs else Left ()
+    extractMask tkns = case tkns of
+      []    -> Left ()
+      (x:_) -> Right $ SetMask x
+    extractAddr tkns = case tkns of
+      []
+      (x:xs)
+    extractInstruction x = case x of
+      Left _            -> Nothing
+      Right instruction -> Just instruction
+    parseSetMask = arr mEq >>> rightarr extractMask >>> arr extractInstruction
+    parseSetMem = arr mSbl >>> rightarr extractAddr >>> rightarr mSbr >>>
+                    rightarr mEq >>> rightarr extractVal >>> arr extractInstruction
+    matchInstruction tkns = case tkns of
+      []     -> Nothing
+      (command:tkns') -> case command of
+        "mask" -> parseSetMask tkns'
+        "mem"  -> parseSetMem tkns'
+        _      -> Nothing
 
 main :: IO ()
 main = do
